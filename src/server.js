e235bd0e330940a62b757ef9c546c0a52a10c4fc
robertcostaa <robertaraujo2004@gmail.com>
2025-11-03 16:08:58 -0300

    edit server

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`API da Barbearia rodando em http://localhost:${PORT}`);
+  console.log(`💈 API da Barbearia rodando na porta ${PORT}`);
 });
+
